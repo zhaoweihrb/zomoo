279b0726706cb836ee1cd6d12baa830919793bfb
zhaoweihrb <zhaoweihrb@users.noreply.github.com>
2026-04-12 12:05:53 +0000

    feat: 拉取主分支
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,26 @@
 pluginManagement {
     repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
+        maven {
+            url = uri("https://maven.aliyun.com/repository/google")
+        }
+        maven {
+            url = uri("https://maven.aliyun.com/repository/central")
+        }
+        maven {
+            url = uri("https://maven.aliyun.com/repository/gradle-plugin")
+        }
     }
 }
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        google()
-        mavenCentral()
+        maven {
+            url = uri("https://maven.aliyun.com/repository/google")
+        }
+        maven {
+            url = uri("https://maven.aliyun.com/repository/central")
+        }
     }
 }
 
