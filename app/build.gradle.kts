2b580629e2f4ceded89608812aba5852419ebca8
zhaoweihrb <zhaoweihrb@users.noreply.github.com>
2026-04-13 09:54:31 +0000

    feat: 拉取主分支
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "8.5.0"
-    id("org.jetbrains.kotlin.android") version "2.0.20"
+    id("com.android.application") version "8.7.2"
+    id("org.jetbrains.kotlin.android") version "2.2.0"
 }
 
 android {
