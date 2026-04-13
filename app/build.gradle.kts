30003d59bd19b19c65df9834fd129ff836c09922
zhaoweihrb <zhaoweihrb@users.noreply.github.com>
2026-04-13 13:04:29 +0000

    feat: 拉取主分支
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "8.7.2"
-    id("org.jetbrains.kotlin.android") version "2.2.0"
+    id("com.android.application") version "8.5.0"
+    id("org.jetbrains.kotlin.android") version "2.0.20"
 }
 
 android {
@@ -27,13 +27,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_21
-        targetCompatibility = JavaVersion.VERSION_21
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlin {
-        compilerOptions {
-            jvmTarget = "21"
-        }
+    kotlinOptions {
+        jvmTarget = "11"
     }
     buildFeatures {
         viewBinding = true
